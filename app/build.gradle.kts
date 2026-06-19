8762bb1d7467fcd8190dac24d6922aadcf23a4bd
2300030811 <2300030811@kluniversity.in>
2026-06-19 06:13:54 +0530

    chore: Release v2.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,8 +43,8 @@ android {
         applicationId = "com.vynce.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 76
-        versionName = "1.1.6"
+        versionCode = 77
+        versionName = "2.0.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         manifestPlaceholders["appAuthRedirectScheme"] = "com.vynce.app"
         
